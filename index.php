15c4bca5db30dd1037e887ab99c8c4fc89b067cf
Mouhcine Essafi <messafi1337@gmail.com>
2023-12-14 23:40:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,13 @@
 </head>
 <body>
 <div class="bg-white">
-    <nav class="bg-gray-900">
+    <nav class="bg-gray-900 fixed w-full top-0 z-10">
       <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="flex items-center justify-between h-16">
-          <div class="flex items-center">
             <div class="flex-shrink-0">
+            <div class="justify-center w-1/5 text-center">
+                  <svg class="w-8 h-8 mx-auto text-gray-200 cursor-pointer fill-current hover:text-gray-300" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 391.23 391.23" xml:space="preserve" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M147.949,95.591c-50.858,19.717-84.835,69.62-84.835,124.178v36.332c0,4.971-4.416,9.127-9.387,9.127H34.966 c-15.307,0-27.852-12.58-27.852-27.887v-41.042c0-12.096,7.786-22.718,19.323-26.432c4.735-1.524,9.837,1.078,11.36,5.809 c1.523,4.731-1.1,9.802-5.832,11.325c-4.007,1.29-6.851,5.114-6.851,9.298v41.042c0,5.382,4.471,9.887,9.852,9.887h10.148v-27.458 c0-31.085,9.174-60.947,26.895-86.357c17.313-24.824,41.129-43.706,69.241-54.604c4.634-1.796,9.848,0.503,11.645,5.139 C154.69,88.581,152.583,93.794,147.949,95.591z M294.179,100.714c0,18.881-15.362,34.243-34.244,34.243 c-18.881,0-34.243-15.361-34.243-34.243s15.361-34.243,34.243-34.243C278.817,66.471,294.179,81.833,294.179,100.714z M276.179,100.714c0-8.956-7.287-16.243-16.244-16.243c-8.956,0-16.243,7.287-16.243,16.243s7.287,16.243,16.243,16.243 C268.892,116.957,276.179,109.67,276.179,100.714z M384.114,196.298v41.042c0,15.307-12.046,27.887-27.353,27.887h-16.623 c-5.459,17-14.122,33.788-25.788,48.499c-19.168,24.173-45.545,42.06-74.879,50.912c-1.319,14.807-13.792,26.588-28.933,26.588 H181.19c-16.017,0-29.048-12.982-29.048-28.999c0-16.02,13.031-29.001,29.048-29.001h29.349c10.542,0,19.791,5.459,24.882,13.885 c55.473-17.165,93.578-68.578,93.578-127.344c0-18.112-3.567-35.647-10.606-52.149l-11.841-5.291 c-5.097,3.869-10.613,7.063-16.487,9.547l-1.474,14.216c-0.485,9.366-11.383,15.156-28.654,15.156 c-17.318,0-28.212-5.806-28.657-15.2l-1.47-14.171c-5.874-2.483-11.39-5.677-16.488-9.546l-13.094,5.852 c-8.351,4.245-18.806-2.298-27.435-17.246c-3.114-5.393-5.342-11.018-6.272-15.835c-1.942-10.064,2.113-14.628,5.086-16.565 l11.633-8.429c-0.378-3.116-0.569-6.261-0.569-9.402c0-3.193,0.197-6.387,0.587-9.55l-11.628-8.424 c-7.915-5.082-7.496-17.421,1.165-32.419c8.63-14.953,19.089-21.496,27.441-17.241l13.22,5.908 c5.056-3.82,10.521-6.977,16.336-9.438l1.488-14.341C231.724,5.807,242.618,0,259.935,0c17.278,0,28.178,5.794,28.655,15.166 l1.49,14.375c5.814,2.46,11.28,5.619,16.337,9.438l13.196-5.895c3.164-1.604,9.145-2.833,16.888,3.878 c3.707,3.214,7.463,7.955,10.577,13.35c8.638,14.957,9.072,27.292,1.201,32.394l-11.664,8.45c0.39,3.163,0.587,6.357,0.587,9.549 c0,3.145-0.19,6.291-0.568,9.401l11.658,8.448c7.858,5.108,7.419,17.44-1.215,32.39c-3.112,5.393-6.869,10.142-10.576,13.357 c-0.004,0.004-0.009,0.024-0.014,0.028c6.978,17.646,10.512,36.31,10.512,55.502c0,9.278-0.819,18.395-2.434,27.395h12.196 c5.382,0,9.353-4.505,9.353-9.887v-41.042c0-4.185-2.646-8.008-6.653-9.298c-4.731-1.523-7.179-6.594-5.656-11.325 c1.523-4.731,6.492-7.333,11.223-5.809C376.566,173.581,384.114,184.203,384.114,196.298z M221.587,362.09 c0-0.735-0.072-1.544-0.209-2.239c-0.024-0.104-0.046-0.115-0.067-0.22c-1.121-4.913-5.525-8.404-10.772-8.404H181.19 c-6.092,0-11.048,4.906-11.048,10.998c0,6.095,4.956,11.002,11.048,11.002h29.349C216.631,373.228,221.587,368.182,221.587,362.09z M321.568,79.831l14.122-10.231c-0.412-2.107-1.613-5.805-4.201-10.286c-2.586-4.48-5.187-7.369-6.806-8.779l-15.983,7.14 c-3.215,1.437-6.974,0.865-9.618-1.462c-5.964-5.25-12.778-9.187-20.25-11.698c-3.343-1.124-5.721-4.096-6.085-7.604l-1.804-17.405 C268.913,18.809,265.11,18,259.935,18c-5.173,0-8.976,0.809-11.007,1.505l-1.806,17.406c-0.364,3.507-2.743,6.479-6.084,7.603 c-7.477,2.513-14.289,6.448-20.249,11.697c-2.644,2.329-6.405,2.9-9.62,1.462l-15.981-7.142c-1.619,1.41-4.221,4.299-6.808,8.78 c-2.587,4.48-3.789,8.178-4.2,10.286l14.122,10.231c2.855,2.069,4.24,5.614,3.542,9.07c-0.78,3.863-1.175,7.835-1.175,11.807 c0,3.927,0.387,7.858,1.151,11.687c0.688,3.45-0.697,6.985-3.545,9.049l-14.094,10.212c0.412,2.107,1.613,5.805,4.2,10.284 c2.587,4.482,5.189,7.371,6.808,8.781l15.86-7.087c3.223-1.441,6.991-0.863,9.636,1.476c5.989,5.299,12.842,9.268,20.368,11.794 c3.344,1.123,5.724,4.095,6.088,7.603l1.789,17.239c2.031,0.697,5.833,1.505,11.007,1.505c5.175,0,8.978-0.809,11.009-1.506 l1.788-17.237c0.364-3.508,2.743-6.48,6.086-7.604c7.528-2.528,14.381-6.497,20.368-11.795c2.645-2.34,6.411-2.917,9.635-1.477 l15.862,7.086c1.619-1.411,4.219-4.299,6.804-8.778c2.589-4.482,3.791-8.181,4.202-10.288l-14.094-10.21 c-2.851-2.065-4.236-5.604-3.544-9.055c0.763-3.813,1.15-7.743,1.15-11.679c0-3.971-0.396-7.943-1.175-11.806 C317.329,85.444,318.713,81.899,321.568,79.831z"></path> </g></svg>
+              </div>
             </div>
             <div class="hidden md:block">
               <div class="flex items-center ml-10">
@@ -26,7 +28,6 @@
                 </a>
               </div>
             </div>
-          </div>
           <div class="hidden md:block">
             <div class="flex items-center ml-4 md:ml-6">
               <button class="p-1 text-gray-400 border-2 border-transparent rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700" aria-label="Notifications">
@@ -69,12 +70,12 @@
             On hold
             <span class="flex items-center justify-center w-6 h-6 p-1 ml-1 text-xs bg-gray-200 rounded-full">38</span>
           </div>
-          <div class="flex items-center px-4 pb-2 text-sm border-b-2 border-transparent cursor-pointer hover:border-gray-300 md:flex md:block">
+          <div class="flex items-center px-4 pb-2 text-sm border-b-2 border-transparent cursor-pointer hover:border-gray-300 md:flex ">
               <path d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"></path></svg>
             Solved
             <span class="flex items-center justify-center w-6 h-6 p-1 ml-1 text-xs bg-gray-200 rounded-full">38</span>
           </div>
-          <div class="flex items-center px-4 pb-2 text-sm border-b-2 border-transparent cursor-pointer hover:border-gray-300 md:block md:flex">
+          <div class="flex items-center px-4 pb-2 text-sm border-b-2 border-transparent cursor-pointer hover:border-gray-300 md:flex">
               <path d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
               Closed
               <span class="flex items-center justify-center w-6 h-6 p-1 ml-1 text-xs bg-gray-200 rounded-full">38</span>
@@ -82,6 +83,112 @@
         </div>
       </div>
     </div>
+
+    <div class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
+  <div class="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
+    <div class="flex items-center justify-center h-14 border-b">
+      <div>Sidebar Navigation By iAmine</div>
+    </div>
+    <div class="overflow-y-auto overflow-x-hidden flex-grow">
+      <ul class="flex flex-col py-4 space-y-1">
+        <li class="px-5">
+          <div class="flex flex-row items-center h-8">
+            <div class="text-sm font-light tracking-wide text-gray-500">Menu</div>
+          </div>
+        </li>
+        <li>
+          <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+            <span class="inline-flex justify-center items-center ml-4">
+              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+            </span>
+            <span class="ml-2 text-sm tracking-wide truncate">Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+            <span class="inline-flex justify-center items-center ml-4">
+              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path></svg>
+            </span>
+            <span class="ml-2 text-sm tracking-wide truncate">Inbox</span>
+            <span class="px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-indigo-500 bg-indigo-50 rounded-full">New</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+            <span class="inline-flex justify-center items-center ml-4">
+              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
+            </span>
+            <span class="ml-2 text-sm tracking-wide truncate">Messages</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+            <span class="inline-flex justify-center items-center ml-4">
+              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
+            </span>
+            <span class="ml-2 text-sm tracking-wide truncate">Notifications</span>
+            <span class="px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-500 bg-red-50 rounded-full">1.2k</span>
+          </a>
+        </li>
+        <li class="px-5">
+          <div class="flex flex-row items-center h-8">
+            <div class="text-sm font-light tracking-wide text-gray-500">Tasks</div>
+          </div>
+        </li>
+        <li>
+          <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+            <span class="inline-flex justify-center items-center ml-4">
+              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
+            </span>
+            <span class="ml-2 text-sm tracking-wide truncate">Available Tasks</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+            <span class="inline-flex justify-center items-center ml-4">
+              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+            </span>
+            <span class="ml-2 text-sm tracking-wide truncate">Clients</span>
+            <span class="px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-green-500 bg-green-50 rounded-full">15</span>
+          </a>
+        </li>
+        <li class="px-5">
+          <div class="flex flex-row items-center h-8">
+            <div class="text-sm font-light tracking-wide text-gray-500">Settings</div>
+          </div>
+        </li>
+        <li>
+          <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+            <span class="inline-flex justify-center items-center ml-4">
+              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
+            </span>
+            <span class="ml-2 text-sm tracking-wide truncate">Profile</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+            <span class="inline-flex justify-center items-center ml-4">
+              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+              </svg>
+            </span>
+            <span class="ml-2 text-sm tracking-wide truncate">Settings</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+            <span class="inline-flex justify-center items-center ml-4">
+              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
+            </span>
+            <span class="ml-2 text-sm tracking-wide truncate">Logout</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
 </div>
 </body>
 </html>
