214a804d134c70075b50448b12464a2f702bd0b9
Mouhcine Essafi <messafi1337@gmail.com>
2023-12-18 11:02:32 +0100

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -2,6 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$db_name = "Helpdesk";
 
 
 /**
@@ -16,5 +17,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . mysqli_connect_error());
     echo ('error: ');
 }
+
+/**
+ * SQL query to create a database
+ */
+$sql = "CREATE DATABASE IF NOT EXISTS $db_name";
+
+if (!$conn->query($sql) === TRUE) {
+    echo "Error creating database: " . $conn->error;
+}
+
+/**
+ * Select your database
+ */
+$conn->select_db($db_name);
 
 ?>
