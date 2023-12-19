40910f6cac6fd34a1da383180b2a54fc3e66ac76
Mouhcine Essafi <messafi1337@gmail.com>
2023-12-19 10:11:43 +0100

    Update register_trait.php

--- trait/register_trait.php
+++ trait/register_trait.php
@@ -4,7 +4,7 @@ include '..\class\user_register.php';
 
 if (isset($_POST['submit'])) {
     $userRegistration = new UserRegistration($db);
-    $registrationResult = $userRegistration->registerUser($_POST['f-name'], $_POST['l-name'], $_POST['email'], $_POST['password']);
+    $registrationResult = $userRegistration->registerUser($_POST['f-name'], $_POST['l-name'], $_POST['reg_email'], $_POST['reg_password']);
     echo "<script>alert('$registrationResult');</script>";
 }
 ?>
