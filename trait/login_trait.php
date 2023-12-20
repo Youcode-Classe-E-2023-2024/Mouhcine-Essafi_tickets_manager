78d59640b5fd97ca6d25cb13f5a2dfb978147027
Mouhcine Essafi <messafi1337@gmail.com>
2023-12-20 09:43:06 +0100

    Update login_trait.php

--- trait/login_trait.php
+++ trait/login_trait.php
@@ -5,6 +5,8 @@ include '..\class\user_login.php';
 if (isset($_POST['submit_l'])) {
     $userRegistration = new UserLogin($db);
     $registrationResult = $userRegistration->loginUser($_POST['log_email'], $_POST['log_password']);
-    echo "<script>alert('$registrationResult');</script>";
+    // echo "<script>alert('$registrationResult');</script>";
+    if ($registrationResult === true) {
+        header('location: ../view/index.php');}
 }
 ?>
